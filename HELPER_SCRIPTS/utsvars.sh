06cdeabde7d5f6442a301e3e85c36bde782fd40d
Mark Collins-Cope <markcollinscope@gmail.com>
2024-11-27 21:14:44 +0000

    <bool.baseline>: first quick hack at bool

--- HELPER_SCRIPTS/utsvars.sh
+++ HELPER_SCRIPTS/utsvars.sh
@@ -22,7 +22,11 @@ main()
 	if $STARTCWD; then ROOT=$(pwd); fi
 	(
 		cd $ROOT;
-		xgrep -r -h UTS_ | sed 's/.*\(UTS_[[:alnum:]]*\).*/\1/' | sort | uniq
+		local results=$(xgrep -r -h UTS_ | sed 's/.*\(UTS_[[:alnum:]]*\).*/\1/' | sort | uniq);
+
+		for r in $results; do 
+			errecho "$r: <${!r}>"
+		done
 	)
 }
 
